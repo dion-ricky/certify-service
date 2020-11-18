524377c15fd264abff3280108db678d5a700797f
Dion Ricky <dionrickysptr@gmail.com>
2020-11-18 14:14:31 +0000

    🚀 #1 using local env and hardcoded config

---
+++ index.js
@@ -0,0 +1,26 @@
+const { createCanvas, registerFont, loadImage, Image } = require('canvas')
+
+registerFont('OpenSans-Regular.ttf', {family: 'Open Sans'})
+
+const canvas = createCanvas(2382, 1684)
+const ctx = canvas.getContext('2d')
+
+const img = new Image()
+img.onload = () => ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+img.onerror = err => { throw err }
+img.src = 'exp-noname-sertif.png'
+
+ctx.font = '100pt "Open Sans"'
+ctx.textAlign = 'center';
+ctx.fillStyle = '#202020';
+
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+
+ctx.fillText("Dion Ricky Saputra", x, y);
+
+const fs = require('fs')
+const out = fs.createWriteStream(__dirname + '/test.png')
+const stream = canvas.createPNGStream()
+stream.pipe(out)
+out.on('finish', () =>  console.log('The PNG file was created.'))
